58a94ab665a8add197a5d8d2aaa5c70ec04cfd0e
Alexis <220240@fppiramide.com>
2023-10-06 14:11:54 +0200

    Me quedo sin cosas que decir sobre la prima de juan

--- src/main/java/com/example/Postman/Dominio/OfertaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Postman.Dominio;
-
-import java.util.List;
-
-public interface OfertaRepository {
-
-    public List<Oferta> getAll();
-    public List<Oferta> getTitulos(Oferta titulo);
-}
